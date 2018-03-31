ebe4ebd1477fc59a19111ff82bf959c2aaf90331
Liu Chao <lc@smartkarma.com>
2018-03-31 08:45:42 +0800

    Array wip 2018-03-31

--- Array/index.js
+++ Array/index.js
@@ -56,5 +56,65 @@ const deepFlatten = (arr) => [].concat(...arr.map(v) => (Arra.isArray(v) ? deepF
 // Create a Set from b, then use Array.filter() on a to only keep values not contained in b.
 const difference = (a, b) => {
   const s = new Set(b);
-  return a.filter((v) => !s.has(v));
+  return a.filter(v => !s.has(v));
 }
+
+// differenceBy
+// Returns the difference between two arrays, after applying the provided function to each array element of both.
+// Create a Set by applying fn to each element in b, then use Array.filter() in combination with fn on a to only keep values not contained in the previously created set.
+const differenceBy = (a, b, fn) => {
+  const s = new Set(b.map(fn));
+  return a.filter(v => !s.has(fn(v)));
+}
+
+// differenceWith
+// Filters out all values from an array for which the comparator function does not return true.
+// Use Array.filter() and Array.findIndex() to find the appropriate values.
+const differenceWith = (arr, val, comp) => arr.filter(a => val.findIndex(b => comp(a, b)) === -1)
+
+// drop
+// Returns a new array with n elements removed from the left.
+// Use Array.slice() to slice the remove the specified number of elements from the left.
+const drop = (arr, n = 1) => arr.slice(n);
+
+// dropRight
+// Returns a new array with n elements removed from the right.
+// Use Array.slice() to slice the remove the specified number of elements from the right.
+const dropRight = (arr, n = 1) => arr.slice(0, -n);
+
+// dropRightWhile
+// Removes elements from the end of an array until the passed function returns true. Returns the remaining elements in the array.
+// Loop through the array, using Array.slice() to drop the last element of the array until the returned value from the function is true. Returns the remaining elements.
+cosnt dropRightWhile = (arr, fn) => {
+  while (arr.length > 0 && !fn(arr[arr.length - 1])) arr = arr.slice(0, -1);
+  return arr;
+};
+
+// dropWhile
+// Removes elements in an array until the passed function returns true. Returns the remaining elements in the array.
+// Loop through the array, using Array.slice() to drop the first element of the array until the returned value from the function is true. Returns the remaining elements.
+const dropWhile = (arr, fn) => {
+  while (arr.length > 0 && !fn(arr[0])) arr = arr.slice(1);
+  return arr;
+};
+
+// everyNth
+// Returns every nth element in an array.
+// Use Array.filter() to create a new array that contains every nth element of a given array.
+const everyNth = (arr, n) => arr.filter((v, i) => i % n === n - 1);
+
+// filterNonUnique
+// Filters out the non-unique values in an array.
+// Use Array.filter() for an array containing only the unique values.
+const filterNonUnique = arr => arr.filter(v => arr.indexOf(v) === arr.lastIndexOf(v));
+
+// findLast
+// Returns the last element for which the provided function returns a truthy value.
+// Use Array.filter() to remove elements for which fn returns falsey values, Array.slice(-1) to get the last one.
+const findLast = (arr, fn) => arr.filter(fn).slice(-1)[0];
+
+// findLastIndex
+// Returns the index of the last element for which the provided function returns a truthy value.
+// Use Array.map() to map each element to an array with its index and value. Use Array.filter() to remove elements for which fn returns falsey values, Array.slice(-1) to get the last one.
+const findLastIndex = (arr, fn) => arr.map((v, i) => [i, v]).filter(val => fn(val[1], val[0], arr)).slice(-1)[0][0]
+
